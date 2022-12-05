44de278c82f44a026892890d0c9e1fc26adc1309
manq2010 <manq2010@gmail.com>
2022-12-05 13:15:36 +0200

    Add react lifecycle methods and persist data to local storage

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -7,24 +7,8 @@ import InputTodo from './InputTodo';
 
 class TodoContainer extends React.Component {
     state = {
-      todos: [
-        {
-          id: uuidv4(),
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: uuidv4(),
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ],
-    };
+        todos: [],
+      }
 
     handleChange = (id) => {
       this.setState((prevState) => ({
@@ -68,6 +52,34 @@ class TodoContainer extends React.Component {
             return todo;
           }),
         });
+      }
+
+      componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+          .then(response => response.json())
+          .then(data => this.setState({ todos: data }));
+      }
+
+      componentDidUpdate(prevProps, prevState) {
+        if(prevState.todos !== this.state.todos) {
+          const temp = JSON.stringify(this.state.todos)
+          localStorage.setItem("todos", temp)
+        }
+      }
+
+
+      componentDidMount() {
+        const temp = localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+        if (loadedTodos) {
+          this.setState({
+            todos: loadedTodos
+          })
+        }
+      }
+
+      componentWillUnmount() {
+        console.log("Cleaning up...")
       }
 
       render() {
