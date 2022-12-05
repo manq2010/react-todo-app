ae653b7d863028d336d99d32a04387e8e464111b
manq2010 <manq2010@gmail.com>
2022-12-05 13:03:35 +0200

    Edit items

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -6,27 +6,27 @@ class InputTodo extends Component {
      title: '',
    };
 
-   onChange = e => {
-    this.setState({
-        [e.target.name]: e.target.value,
-      });
-  };
+   onChange = (e) => {
+     this.setState({
+       [e.target.name]: e.target.value,
+     });
+   };
 
-  handleSubmit = e => {
-    e.preventDefault()
+  handleSubmit = (e) => {
+    e.preventDefault();
     if (this.state.title.trim()) {
-      this.props.addTodoProps(this.state.title)
+      this.props.addTodoProps(this.state.title);
       this.setState({
-        title: "",
-      })
+        title: '',
+      });
     } else {
-      alert("Please write item")
+      alert('Please write item');
     }
   }
 
-   render() {
-     return (
-        <form onSubmit={this.handleSubmit} className="form-container">
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
           type="text"
           className="input-text"
@@ -37,7 +37,7 @@ class InputTodo extends Component {
         />
         <button className="input-submit">Submit</button>
       </form>
-     );
-   }
+    );
+  }
 }
 export default InputTodo;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,26 +3,64 @@ import React, { Component } from 'react';
 import styles from './TodoItem.module.css';
 
 export default class TodoItem extends Component {
-  render() {
-    const completedStyle = {
-      fontStyle: 'italic',
-      color: '#595959',
-      opacity: 0.4,
-      textDecoration: 'line-through',
-    };
-    //   Using Destructuring
-    const { completed, id, title } = this.props.todo;
-    return (
-      <li className={styles.item}>
-        <input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
-        />
-        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>{title}</span>
-      </li>
-    );
-  }
+    state = {
+      editing: false,
+    }
+
+      handleEditing = () => {
+        this.setState({
+          editing: true,
+        });
+      }
+
+      handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+          this.setState({ editing: false })
+        }
+      }
+
+      render() {
+        // add the logic that dynamically hides/display the todos/ text fi
+        const viewMode = {};
+        const editMode = {};
+
+        if (this.state.editing) {
+          viewMode.display = 'none';
+        } else {
+          editMode.display = 'none';
+        }
+
+        const completedStyle = {
+          fontStyle: 'italic',
+          color: '#595959',
+          opacity: 0.4,
+          textDecoration: 'line-through',
+        };
+        //   Using Destructuring
+        const { completed, id, title } = this.props.todo;
+        return (
+          <li className={styles.item}>
+            <div onDoubleClick={this.handleEditing} style={viewMode}>
+              <input
+                type="checkbox"
+                className={styles.checkbox}
+                checked={completed}
+                onChange={() => this.props.handleChangeProps(id)}
+              />
+              <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+              <span style={completed ? completedStyle : null}>{title}</span>
+            </div>
+            <input
+              type="text"
+              style={editMode}
+              className={styles.textInput}
+              value={title}
+              onChange={(e) => {
+                this.props.setUpdate(e.target.value, id);
+              }}
+              onKeyDown={this.handleUpdatedDone}
+            />
+          </li>
+        );
+      }
 }
