1ca16b43bc72a5c902d0e6a899c24b28bf98b394
manq2010 <manq2010@gmail.com>
2022-12-05 12:48:55 +0200

    Add destructuring

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,30 +1,28 @@
 /* eslint-disable*/
 import React, { Component } from 'react';
-import styles from "./TodoItem.module.css";
+import styles from './TodoItem.module.css';
 
 export default class TodoItem extends Component {
   render() {
     const completedStyle = {
-        fontStyle: "italic",
-        color: "#595959",
-        opacity: 0.4,
-        textDecoration: "line-through",
-      }
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+    //   Using Destructuring
+    const { completed, id, title } = this.props.todo;
     return (
-        <li className={styles.item}>
+      <li className={styles.item}>
         <input
           type="checkbox"
           className={styles.checkbox}
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
         />
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-          Delete
-        </button>
-        <span style={this.props.todo.completed ? completedStyle : null}>
-          {this.props.todo.title}
-        </span>
+        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>{title}</span>
       </li>
-      )
+    );
   }
 }
