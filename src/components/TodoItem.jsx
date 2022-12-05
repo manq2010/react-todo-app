14472b66c5911b000458314762bd9af16ec7cc9a
manq2010 <manq2010@gmail.com>
2022-12-05 11:52:23 +0200

    Raise and Handle Events

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,6 +3,13 @@ import React, { Component } from 'react'
 
 export default class TodoItem extends Component {
   render() {
-    return <li>{this.props.todo.title}</li>
+    return <li>
+       <input
+            type="checkbox"
+            checked={this.props.todo.completed}
+            onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+/>
+        {this.props.todo.title}
+        </li>
   }
 }
