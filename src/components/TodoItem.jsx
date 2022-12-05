1da39dd5b14998462eb3709135e906e04c784124
manq2010 <manq2010@gmail.com>
2022-12-05 12:28:40 +0200

    Delete and add text input

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -8,7 +8,10 @@ export default class TodoItem extends Component {
             type="checkbox"
             checked={this.props.todo.completed}
             onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-/>
+        /> 
+       
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}> Delete
+        </button>
         {this.props.todo.title}
         </li>
   }
